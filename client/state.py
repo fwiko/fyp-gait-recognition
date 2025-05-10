bbafc3f6f3c96685105c686e21c8cb22cbd6ee19
Rafferty Simms <raffsimms23@gmail.com>
2025-05-10 12:00:25 +0100

    Root-level restructure

--- client/state.py
+++
@@ -1,7 +0,0 @@
-state = {
-    "background_init": False, # Whether the gait processor's background model has been initialised
-    "reset_requested": False, # Whether a reset has been requested
-    "frame_counter": 0, # Number of frames since the last GEI classification attempt
-    "gei_buffer": None, # Most recent GEI
-    "last_saved_gei": None, # The most recent GEI transmitted
-}
